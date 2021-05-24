7d15f5dc136df26e3c3b2fb74791d311e754a921
morganfula <morgan.fula@me.com>
2021-05-24 08:55:03 +0200

    fixes

--- assets/config.js
+++ assets/config.js
@@ -13,7 +13,7 @@ export default {
   // these are the rest api endpoints and your wordpress url
   client: `https://${apiEndpoint}`,
   wpDomain: `https://${apiEndpoint}/wp-json`,
-  url: "https://downtown-dinner.netlify.app/", // your website url
+  url: "https://www.downtown-dinner.ch/", // your website url
   loadDbName: "[starter_wp]", // db name if needed
   api: {
     homePage: "/wp/v2/pages/357",
